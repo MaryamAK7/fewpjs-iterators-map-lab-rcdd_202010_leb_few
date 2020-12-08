64e6821e247940d4cd1d0cb63cd84563339bf297
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-08 16:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ const tutorials = [
 const titleCased = (tutorials) => {
   return tutorials.map((elem)=>{
     let arr= elem.split(' ');
-  for(let i=0;i<arr.length;i++){
-    arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
-  }
-  return arr.join(' ')})
+    for(let i=0;i<arr.length;i++){
+     arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+    }
+  return arr.join(' ')
+    
+  })
 }
 
 console.log(titleCased(tutorials))
